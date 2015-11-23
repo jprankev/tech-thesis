a5fe8557a9065728d2b6ee02b36ffa3d39e4582d
jprankev <jprankev@gmail.com>
2015-11-23 15:28:45 -0600

    Dummy change

--- function_prototypes.h
+++ function_prototypes.h
@@ -8,7 +8,7 @@ void buttons(void);
 void interrupt low_priority TOGGLE_SEQUENCE(void);
 void INT_INIT(void);
 void SEND_NOTE(void);
-void sequence_up(void);
+void sequence_up(void)
 void sequence_down(void);
 void SEND_NOTEOFF(void);
 void SEND_NOTEOFF_DOWN(void);
